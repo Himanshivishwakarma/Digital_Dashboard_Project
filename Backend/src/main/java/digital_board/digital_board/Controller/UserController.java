df89e306317a38cdeb5b7af9f8ba636d0617ed6c
sahilkhan89 <sahilk.bca2021@ssism.org>
2023-10-30 16:49:29 +0530

    test

--- Backend/src/main/java/digital_board/digital_board/Controller/UserController.java
+++ Backend/src/main/java/digital_board/digital_board/Controller/UserController.java
@@ -14,19 +14,6 @@ import digital_board.digital_board.Dto.AuthResponse;
 public class UserController  {
 
 
-    @GetMapping("/test")
-    public ResponseEntity<AuthResponse> home(@AuthenticationPrincipal OidcUser principal) {
-        AuthResponse authResponse = new AuthResponse();
-         authResponse.setName(principal.getEmail());
-        authResponse.setToken(principal.getIdToken().getTokenValue());
-        return ResponseEntity.ok(authResponse);
-    }
-
-      @GetMapping("/public")
-    public String publicTest() {
-        return "working";
-    }
-
 
 
  
