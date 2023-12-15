1b6d52c0d040f97af339763d6e8f2f0ad221135b
mangalsingh123t <121483110+mangalsingh123t@users.noreply.github.com>
2023-12-15 15:10:34 +0530

    Update the UserNotification

--- Backend/src/main/java/digital_board/digital_board/ServiceImpl/UserNotificationServiceImpl.java
+++ Backend/src/main/java/digital_board/digital_board/ServiceImpl/UserNotificationServiceImpl.java
@@ -76,4 +76,6 @@ public class UserNotificationServiceImpl implements UserNotificationService {
         throw new UnsupportedOperationException("Unimplemented method 'getUserNotificationByDepartment'");
     }
 
+   
+
 }
